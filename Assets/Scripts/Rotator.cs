bfa6317bbe57ec5fe7bc3524d9c42ecaa6fc0f33
MilanVlad <milan.vladisavljevic487@education.nsw.gov.au>
2025-03-20 09:40:46 +1100

    .

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+<<<<<<< HEAD
+=======
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+>>>>>>> 768601ce45e52046f7e0ab62b2b6c82644c35f90
 
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+=======
+        
+>>>>>>> 768601ce45e52046f7e0ab62b2b6c82644c35f90
     }
 }
